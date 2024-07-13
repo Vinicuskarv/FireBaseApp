6c68847e3b5c3aa5870939b94e604a2eb38a6efd
Vinicius <karvvinicius@gmail.com>
2024-07-13 19:46:55 +0100

    add style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
-import { useState } from 'react';
-import { db } from './firebaseConnection';
-import { collection, doc, setDoc, getDocs } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
+import { db, auth } from './firebaseConnection';
+import { collection, doc, setDoc, getDocs, updateDoc, deleteDoc, onSnapshot } from 'firebase/firestore';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
 
 import './app.css';
 
 function App() {
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
+  const [idPost, setIdPost] = useState('');
+
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const [user, setUser] = useState(false);
+  const [userDetail, setUserDetail] = useState([]);
+
   const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "posts"), (querySnapshot) => {
+      let listaPost = [];
+
+      querySnapshot.forEach((doc) => {
+        listaPost.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor,
+        });
+      });
+
+      setPosts(listaPost);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    async function checkLogin(){
+      onAuthStateChanged(auth, (user) => {
+        if(user){
+          
+          setUser(true)
+          setUserDetail({
+            uid: user.uid,
+            email: user.email,
+          })
+          
+        }else{
+
+          setUser(false)
+          setUserDetail({})
+        }
+      })
+    }
+
+    checkLogin();
+  })
 
   async function handleAdd() {
     try {
@@ -44,11 +93,127 @@ function App() {
     }
   }
 
+  async function editarPost() {
+    const docRef = doc(db, "posts", idPost);
+
+    await updateDoc(docRef, {
+      titulo: titulo,
+      autor: autor,
+    })
+    .then(() => {
+      console.log("Post atualizado!");
+      setIdPost('');
+      setAutor('');
+      setTitulo('');
+    })
+    .catch(() => {
+      console.log("Erro ao atualizar o post");
+    });
+  }
+
+  async function excluirPost(id) {
+    const docRef = doc(db, "posts", id);
+    await deleteDoc(docRef)
+    .then(() => {
+      alert("Post deletado com sucesso!");
+    })
+    .catch((error) => {
+      console.log("Erro ao deletar o post: " + error);
+    });
+  }
+
+  async function novoUsuario(){
+    await createUserWithEmailAndPassword(auth, email, senha)
+    .then(() => {
+      console.log("Cadastrado com sucesso!")
+      setEmail('')
+      setSenha('')
+    })
+    .catch((error) => {
+      if(error.code === 'auth/weak-password'){
+        alert("Senha muito fraca.")
+      }else if(error.code === 'auth/email-already-in-use'){
+        alert("Email já existe!")
+      }
+    })
+
+  }
+  async function logarUsuario(){
+    await signInWithEmailAndPassword(auth, email, senha)
+
+    .then((value) => {
+      console.log("User Logado com sucesso!")
+
+
+      setUserDetail({
+        uid:value.user.uid,
+        email:value.user.email,
+      })
+      setUser(true)
+
+      setEmail('')
+      setSenha('')
+    })
+    .catch((error) => {
+      console.log("Erro ao fazer login")
+    })
+  }
+
+  async function fazerLogout(){
+    await signOut(auth)
+    setUser(false)
+    setUserDetail({})
+  }
+
   return (
     <div>
       <h1>React + Firebase</h1>
 
+      { user && (
+        <div className='container'>
+          <strong>Seja bem-vindo(a) (Você está logado!)</strong>
+          <span>ID: {userDetail.uid} - Email: {userDetail.email}</span>
+          <button onClick={fazerLogout}>Sair da conta</button>
+          <br/><br/>
+        </div>
+      )}
+
       <div className='container'>
+      <h2>Usuarios</h2>
+
+        <label>Email:</label>
+        <input
+          placeholder='Digite um email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        /><br/>
+        <label>Senha:</label>
+        <input
+          placeholder='Informe sua senha'
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          type='password'
+        /><br/>
+
+        <button onClick={novoUsuario}>Cadastrar</button>
+        <button onClick={logarUsuario}>Fazer Login</button>
+
+      </div>
+      
+
+      
+      <hr/>
+      
+
+      <div className='container'>
+        <h2>Posts</h2>
+        <label>ID do Post:</label>
+        <input
+          placeholder='Digite o ID do post'
+          value={idPost}
+          onChange={(e) => setIdPost(e.target.value)}
+        /><br/>
+
         <label>Título:</label>
         <textarea
           placeholder='Digite o título'
@@ -64,14 +229,19 @@ function App() {
         />
 
         <button onClick={handleAdd}>Cadastrar</button>
-        <button onClick={buscarPosts}>Buscar posts</button>
+        <button onClick={buscarPosts}>Buscar posts</button><br/>
+
+        <button onClick={editarPost}>Atualizar post</button>
+
 
         <ul>
           {posts.map((post) => {
             return (
               <li key={post.id}>
+                <span>ID: {post.id}</span><br/>
                 <span>Título: {post.titulo}</span><br/>
                 <span>Autor: {post.autor}</span><br/>
+                <button onClick={() => excluirPost(post.id)}>Excluir</button><br/><br/>
               </li>
             );
           })}
--- src/firebaseConnection.js
+++ src/firebaseConnection.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -12,6 +13,8 @@ const firebaseConfig = {
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
 
-export { db };
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp)
+
+export { db, auth };
